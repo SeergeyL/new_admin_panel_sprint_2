02bb3278135e3f1c80ad12bc47f6b21bee6c60a9
SeergeyL <pstydy@inbox.ru>
2022-04-20 20:36:43 +0300

    add movies list view

--- backend/movies/api/v1/views.py
+++ backend/movies/api/v1/views.py
@@ -1,9 +1,51 @@
+from django.contrib.postgres.aggregates import ArrayAgg
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.list import BaseListView
+from django.db.models import Q
+
+from movies.models import Filmwork
 
 
-class MoviesListApi(View):
+class MoviesListApi(BaseListView):
+    model = Filmwork
     http_method_names = ['get']
+    paginate_by = 50
 
-    def get(self, request, *args, **kwargs):
-        return JsonResponse({}) 
+    def get_queryset(self):
+        qs = Filmwork.objects.prefetch_related('genres', 'persons')
+        qs = qs.annotate(
+            genre=ArrayAgg('genres__name', distinct=True),
+            actors=ArrayAgg('persons__full_name', distinct=True,
+                filter=Q(personfilmwork__role='actor')),
+            writers=ArrayAgg('persons__full_name', distinct=True,
+                filter=Q(personfilmwork__role='writer')),
+            directors=ArrayAgg('persons__full_name', distinct=True,
+                filter=Q(personfilmwork__role='director'))
+        )
+        return qs.values(
+            'id',
+            'title',
+            'description',
+            'creation_date',
+            'rating',
+            'type',
+            'genre',
+            'actors',
+            'writers',
+            'directors'
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator, page, queryset, _ = self.paginate_queryset(queryset, self.paginate_by)
+        context = {
+            'count': paginator.count,
+            'total_pages': paginator.num_pages,
+            'prev': page.number,
+            'next': page.next_page_number() if page.has_next() else None,
+            'results': list(queryset)
+        }
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
